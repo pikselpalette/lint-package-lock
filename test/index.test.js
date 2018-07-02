06764c74209b83edf39d78974bb13bb0afae4bbb
Sam Boylett <sam.boylett@piksel.com>
2018-07-02 15:19:26 +0100

    Add test for using args

--- test/index.test.js
+++ test/index.test.js
@@ -27,3 +27,25 @@ const useFixture = name => process.chdir(path.join(__dirname, 'fixtures', name))
     });
   });
 });
+
+describe('when using --file and --config arg', () => {
+  beforeEach(() => {
+    process.chdir(__dirname);
+  });
+
+  ['custom', 'npm'].forEach((registry) => {
+    describe(`with ${registry} registry and no issues`, () => {
+      it('passes', async () => {
+        await expect(exec(`node ../index.js --config=fixtures/${registry}-registry-pass/.lint-package-lock --file=fixtures/${registry}-registry-pass/package-lock.json`))
+          .resolves.toEqual(expect.any(Object));
+      });
+    });
+
+    describe(`with ${registry} registry and issues`, () => {
+      it('fails', async () => {
+        await expect(exec(`node ../index.js --config=fixtures/${registry}-registry-fail/.lint-package-lock --file=fixtures/${registry}-registry-fail/package-lock.json`))
+          .rejects.toEqual(expect.any(Object));
+      });
+    });
+  });
+});
